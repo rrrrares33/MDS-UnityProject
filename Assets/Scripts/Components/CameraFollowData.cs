4c8276e453d0b04d1143e5c2eb7853dc8ce74b87
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2021-05-19 01:48:30 +0300

    Add Player animations

--- Assets/Scripts/Components/CameraFollowData.cs
+++ Assets/Scripts/Components/CameraFollowData.cs
@@ -2,7 +2,7 @@
 using UnityEngine;
 
 [GenerateAuthoringComponent]
-public class FollowCameraData : IComponentData
+public class CameraFollowData : IComponentData
 {
     public Camera Value;
 }
--- Assets/Scripts/Systems/CameraFollowSystem.cs
+++ Assets/Scripts/Systems/CameraFollowSystem.cs
@@ -3,13 +3,14 @@ using Unity.Jobs;
 using Unity.Transforms;
 using UnityEngine;
 
+[AlwaysSynchronizeSystem]
 public class CameraFollowSystem : JobComponentSystem
 {
     protected override JobHandle OnUpdate(JobHandle inputDeps)
     {
-        Entities.WithAll<PlayerTag>().ForEach((FollowCameraData cameraData, in Translation playerTranslation) =>
+        Entities.WithAll<PlayerTag>().ForEach((CameraFollowData cameraData, in Translation translation) =>
         {
-            cameraData.Value.transform.position = new Vector3(playerTranslation.Value.x, playerTranslation.Value.y, -1);
+            cameraData.Value.transform.position = new Vector3(translation.Value.x, translation.Value.y, -1);
         }).WithoutBurst().Run();
 
         return default;
