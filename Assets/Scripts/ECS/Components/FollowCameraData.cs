66e530b34057e0cb01c9b6fef90ff79530d25283
bUsernameIsUnavailable <61911973+bUsernameIsUnavailable@users.noreply.github.com>
2021-05-18 17:14:33 +0300

    Add Camera Follow system

---
+++ Assets/Scripts/ECS/Components/FollowCameraData.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[GenerateAuthoringComponent]
+public class FollowCameraData : IComponentData
+{
+    public Camera Value;
+}
---
+++ Assets/Scripts/ECS/Systems/CameraFollowSystem.cs
@@ -0,0 +1,17 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using Unity.Transforms;
+using UnityEngine;
+
+public class CameraFollowSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        Entities.WithAll<PlayerTag>().ForEach((FollowCameraData cameraData, in Translation playerTranslation) =>
+        {
+            cameraData.Value.transform.position = new Vector3(playerTranslation.Value.x, playerTranslation.Value.y, -1);
+        }).WithoutBurst().Run();
+
+        return default;
+    }
+}
