1faa0fac249b94caeb703cb3e6a2ae7f134951e1
petrinopopa12 <petrino.popa@yahoo.com>
2021-06-13 15:10:31 +0300

    HealthBar and LifeCount script update

--- Assets/Scripts/Gameplay/HealthBar.cs
+++ Assets/Scripts/Gameplay/HealthBar.cs
@@ -1,18 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image fillBar;
+    public float health;
+
+    public void LoseHealth(int value)
     {
-        
+        if (health <= 0)
+            return;
+
+        health = health - value;
+        fillBar.fillAmount = health / 100;
+
+        if(health<=0)
+        {
+            Debug.Log("YOU DIED");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //    LoseHealth(25);
     }
 }
--- Assets/Scripts/Gameplay/LifeCount.cs
+++ Assets/Scripts/Gameplay/LifeCount.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LifeCount : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Image[] lives;
+    public int livesRemaining;
+
+    public void LoseLife()
     {
-        
+        if (livesRemaining == 0)
+            return;
+        livesRemaining--;
+        lives[livesRemaining].enabled = false;
+
+        if (livesRemaining == 0)
+        {
+            Debug.Log("YOU LOST");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //if (Input.GetKeyDown(KeyCode.Return))
+        //    LoseLife();
     }
 }
